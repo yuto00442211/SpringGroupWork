0d2736ed9380082cefcd41e58c8a27a71c06e8c8
HiranoHayata <yamatotakeru002@gmail.com>
2023-09-11 14:57:00 +0900

    aaaa

--- src/main/java/com/example/demo/WebSecurity/webSecurity.java
+++ src/main/java/com/example/demo/WebSecurity/webSecurity.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+
 @Configuration
 @EnableWebSecurity
 public class webSecurity {
