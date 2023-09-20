a5795cf1b0bec18ac0bb46c6e2f0cba027f26b4e
yuto00442211 <140584874+yuto00442211@users.noreply.github.com>
2023-09-20 22:28:19 +0900

    微調整

--- src/main/java/com/example/demo/Form/BitForm.java
+++ src/main/java/com/example/demo/Form/BitForm.java
@@ -1,10 +1,8 @@
 package com.example.demo.Form;
 
-import jakarta.validation.constraints.Min;
-
 public class BitForm {
     private int productId;
-    @Min(value = 1, message = "入札額は1以上で入力してください")
+   
     private int bidAmount;
 
     // getters and setters
